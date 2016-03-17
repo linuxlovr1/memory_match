e74d8fbe8fd76ebd4f7c9339c54170921ae95718
Parker Smith <linuxlovr1@gmail.com>
2016-03-17 13:20:25 -0600

    first commit

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+
+
+$("#1").click(function() {
+  $(this).addClass('red')});
+
+$("#1").dblclick(function() {
+  $(this).removeClass('red')});
+
+$("#2").click(function() {
+  $(this).addClass('blue')});
+
+$("#2").dblclick(function() {
+  $(this).removeClass('blue')});
+
+$("#3").click(function() {
+  $(this).addClass('orange')});
+
+$("#3").dblclick(function() {
+  $(this).removeClass('orange')});
+
+$("#4").click(function() {
+  $(this).addClass('green')});
+
+$("#4").dblclick(function() {
+  $(this).removeClass('green')});
+
+$("#5").click(function() {
+  $(this).addClass('blue')});
+
+$("#5").dblclick(function() {
+  $(this).removeClass('blue')});
+
+$("#6").click(function() {
+  $(this).addClass('red')});
+
+$("#6").dblclick(function() {
+  $(this).removeClass('red')});
+
+$("#7").click(function() {
+  $(this).addClass('gray')});
+
+$("#7").dblclick(function() {
+  $(this).removeClass('gray')});
+
+$("#8").click(function() {
+  $(this).addClass('gray')});
+
+$("#8").dblclick(function() {
+  $(this).removeClass('gray')});
+
+$("#9").click(function() {
+  $(this).addClass('green')});
+
+$("#9").dblclick(function() {
+  $(this).removeClass('green')});
+
+$("#10").click(function() {
+  $(this).addClass('orange')});
+
+$("#10").dblclick(function() {
+  $(this).removeClass('orange')});
+
+$("#11").click(function() {
+  $(this).addClass('black')});
+
+$("#11").dblclick(function() {
+  $(this).removeClass('black')});
+
+$("#12").click(function() {
+  $(this).addClass('black')});
+
+$("#12").dblclick(function() {
+  $(this).removeClass('black')});
+
+
+
+
+
+
+
+
+
+
+
+
+});
